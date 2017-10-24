5529febef329662c2afabf61d3cc0681d08da4a3
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-10-24 12:22:01 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root "jobs#index"
   namespace :admin do
     resources :jobs do
+      resources :resumes
       member do
         post :open
         post :close
