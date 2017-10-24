0772c9dde598700c90bafaa8df30c126b3b92007
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-10-24 08:54:06 +0800

    implement job crud

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
   before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   # GET /jobs
